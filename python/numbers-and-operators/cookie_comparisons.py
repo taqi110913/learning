f39416b6f67a04430aaa38591b47e77a6765615f
taqi110913 <190168955+taqi110913@users.noreply.github.com>
2025-11-01 10:47:29 +0800

    Initial commit

---
+++ python/numbers-and-operators/cookie_comparisons.py
@@ -0,0 +1,4 @@
+teddy_chocolate_chips = 9
+dolores_chocolate_chips = 13
+print(f"Teddy has {teddy_chocolate_chips} chocolate chips on her cookies, while Dolore has {dolores_chocolate_chips}.")
+print(f"Teddy thinks her cookies has more chocolate chips in it than Dolore's. This is {teddy_chocolate_chips > dolores_chocolate_chips}!")
